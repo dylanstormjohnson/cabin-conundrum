59a613fc95f33aabc8598d02c23e09450df99523
bllongbeverly <bllongbeverly@gmail.com>
2023-05-29 10:50:43 -0500

    added database; started bootstrap in handlebars

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,9 @@
-DROP DATABASE IF EXISTS cabin_conundrum;
-CREATE DATABASE cabin_conundrum;
+CREATE DATABASE IF NOT EXISTS cabin_conundrum_db;
+
+USE cabin_conundrum_db;
+
+CREATE TABLE inventory (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
