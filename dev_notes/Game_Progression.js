1fbf6214e9641ed339ab8d03fd7869aff7ad74af
BMO <117410264+beaunaturale@users.noreply.github.com>
2023-05-31 18:32:18 -0500

    added some notes to game prgress, and created a game.handlebars file

---
+++ dev_notes/Game_Progression.js
@@ -0,0 +1,21 @@
+// TODO: These are the actions the player will need to take to complete the game:
+//  BASEMENT TASKS
+//    1. Pick up Basement_Key from Kitchen
+//    2. Use Basement_Key on Trapdoor_Closed (Trapdoor_Closed is swapped for Trapdoor_Opened)
+//    3. Pick up Shovel in Basement
+//    4. Use Shovel on Dirt_Mound (Dirt_Mound is swapped for Dirt_Mound_Dug and Drawer_Key appears on top of it)
+//    5. Pick up Drawer_Key from Dirt_Mound_Dug in Basement
+//  KITCHEN TASKS
+//    6. Use Drawer_Key on Drawer_Closed in Kitchen (Drawer_Closed is swapped for Drawer_Opened and Note_With_Passcode appears on top of it)
+//    7. Pick up Note_With_Passcode from Drawer_Opened in Kitchen
+//  RETURN TO BASEMENT
+//    8. Use Note_With_Passcode on Safe_Closed in Basement (Safe_Closed is swapped for Safe_Opened and Rope appears on top of it)
+//    9. Pick up Rope from Safe_Opened in Basement
+//  RETURN TO KITCHEN
+//  PROCEED TO LIVING ROOM
+//  LIVING ROOM TASKS
+//    10. Use Rope on Axe in Living_Room (Axe falls to ground with animejs)
+//    11. Pick up Axe in Living_Room
+//    12. Use Axe on Hardwood_Door (Hardwood_Door is swapped for Hardwood_Door_Broken)
+//    13. Click on Hardwood_Door_Broken
+//  ESCAPE
