3edb87a26a07e8cc86d5416c325ce05e8f5e9b28
BMO <117410264+beaunaturale@users.noreply.github.com>
2023-05-31 20:37:39 -0500

    adding gameRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const highScoreRoutes = require('./highScoreRoutes');
+const gameRoutes = require('./gameRoutes')
 
 router.use('/users', userRoutes);
 router.use('/scores', highScoreRoutes);
+router.use('/game', gameRoutes);
 
 module.exports = router;
