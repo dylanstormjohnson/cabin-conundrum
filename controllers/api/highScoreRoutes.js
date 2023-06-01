0b5d013dccfe0f875960ff490353156ded75231b
NoemiCouch <Noemicouch@gmail.com>
2023-05-31 21:15:52 -0500

    were able to get only 5 highscores

--- controllers/api/highScoreRoutes.js
+++ controllers/api/highScoreRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { HighScore, User } = require('../../models');
+const { HighScore } = require('../../models');
 
 // add scores
 router.post('/', async (req, res) => {
@@ -7,37 +7,6 @@ router.post('/', async (req, res) => {
     // req.body = {user_id, time}
     const score = await HighScore.create(req.body);
     res.json(score);
-  } catch (err) {
-    console.log(err);
-    res.status(400).json(err);
-  }
-});
-
-// get all scores
-router.get('/', async (req, res) => {
-  try {
-    const scores = await HighScore.findAll({
-      attributes: ['id', 'user_id', 'time'],
-      include: [{ model: User, attributes: ['username'] }],
-    });
-    const higScores = scores.map((s) => s.get({ plain: true }));
-    res.json(higScores);
-  } catch (err) {
-    console.log(err);
-    res.status(400).json(err);
-  }
-});
-
-// get specific user scores
-router.get('/:userId', async (req, res) => {
-  try {
-    const scores = await HighScore.findAll({
-      where: { user_id: req.params.userId },
-      attributes: ['id', 'user_id', 'time'],
-      include: [{ model: User, attributes: ['username'] }],
-    });
-    const userScores = scores.map((s) => s.get({ plain: true }));
-    res.json(userScores);
   } catch (err) {
     console.log(err);
     res.status(400).json(err);
