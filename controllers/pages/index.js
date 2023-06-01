102d0751450cd8f9ba9dd475f7211a1e91ec314d
NoemiCouch <Noemicouch@gmail.com>
2023-06-01 14:23:00 -0500

    worked on welcome page

--- controllers/pages/index.js
+++ controllers/pages/index.js
@@ -16,7 +16,7 @@ router.use('/highscores', withAuth, highscores);
 router.use('/login', login);
 
 router.get('/', (req, res) => {
-  return res.render('welcome');
+  res.render('welcome', { logged_in: req.session.logged_in });
 });
 
 module.exports = router;
