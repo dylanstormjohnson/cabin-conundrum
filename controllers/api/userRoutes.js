a12135574049a8e5bc2395a170f5bddb622fde38
NoemiCouch <Noemicouch@gmail.com>
2023-06-01 20:59:34 -0500

    update username and password backend working

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -71,6 +71,71 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// update username
+router.put('/username/:id', async (req, res) => {
+  try {
+    const { username } = req.body;
+
+    if (!username) {
+      res.status(400).json({ message: 'Username cannot be empty' });
+      return;
+    }
+
+    const userData = await User.findOne({
+      where: { username },
+    });
+
+    if (userData) {
+      res
+        .status(400)
+        .json({ message: 'Username already exists, use a different one' });
+      return;
+    }
+
+    await User.update(
+      { username },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    res.json({ message: 'Username updated successfully!' });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+// update password
+router.put('/password/:id', async (req, res) => {
+  try {
+    const { password } = req.body;
+
+    // check if passwords length > 8
+    if (password.length < 8)
+      return res
+        .status(400)
+        .json({ message: 'Password must be greater or equal to 8 characters' });
+
+    await User.update(
+      { password },
+      {
+        where: {
+          id: req.params.id,
+        },
+        individualHooks: true,
+      }
+    );
+
+    res.json({ message: 'password updated successfully!' });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
 // logout
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
